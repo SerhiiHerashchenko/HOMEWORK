a34db23c76ece12d8be02d770e3c6b08fd8f292d
SerhiiHerashchenko <serhii.herashchenko@stud.onu.edu.ua>
2024-09-24 22:39:35 +0300

    Update main.java

--- Java/Lab2_ArrayList/main.java
+++ Java/Lab2_ArrayList/main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-class MyComparator implements Comparator{
+class MyComparator<T> implements Comparator<T>{
     @Override
     public int compare(Object o1, Object o2) {
         String s1 = o1.toString();
@@ -17,6 +17,7 @@ class MyComparator implements Comparator{
 class MainClass<T>{
     public static void main(String[] args){
         // #2
+        System.out.println("TASK 2:");
         ArrayList<String> colors = new ArrayList<String>();
         colors.add("Blue");
         colors.add("White");
@@ -26,24 +27,43 @@ class MainClass<T>{
         for (String color : colors) {
             System.out.println(color);
         }
-        System.out.println("\n");
+        System.out.println("");
 
         // #3
+        System.out.println("TASK 3:");
+
         colors.add(1, "Red");
         System.out.println(colors.get(1));
 
+        System.out.println("");
+
         // #4
+        System.out.println("TASK 4:");
+
         String task4String = colors.get(3);
         System.out.println(task4String);
 
+        System.out.println("");
         // #5
-        String newString = "Black";
-        colors.set(2, newString);
+        System.out.println("TASK 5:");
 
+        String newString = "Black";
+        System.out.println("Before: " + colors);
+        colors.set(2, newString);
+        System.out.println("After: " + colors);
+
+        System.out.println("");
         // #6
+        System.out.println("TASK 6:");
+
         colors.remove(3);
+        System.out.println(colors);
+
+        System.out.println("");
 
         // #7
+        System.out.println("TASK 7:");
+
         String soughString = "White";
         if(colors.contains(soughString)){
             System.out.println("Color " + soughString + " are in colors!");
@@ -51,77 +71,69 @@ class MainClass<T>{
         else
             System.out.println("Color " + soughString + " are NOT in colors!");
 
+        System.out.println("");
+
         // #8
+        System.out.println("TASK 8:");
+
         System.out.println("Before " + colors);
         Collections.sort(colors, new MyComparator());
         System.out.println("After " + colors);
 
+        System.out.println("");
+
         // #9
-        ArrayList<String> copyColors = colors;
+        System.out.println("TASK 9:");
+
+        ArrayList<String> copyColors = new ArrayList<String>(colors);;
         System.out.println(copyColors);
 
+        System.out.println("");
+
         // #10
-        ArrayList<String> reversedColors = reverse(colors);
+        System.out.println("TASK 10:");
+
+        ArrayList<String> reversedColors = new ArrayList<String>(colors);
+        Collections.reverse(reversedColors);
         System.out.println(reversedColors);
 
+        System.out.println("");
+
         // #11
-        System.out.println(compare(colors, reversedColors));
+        System.out.println("TASK 11:");
 
-        ArrayList<Integer> integers = new ArrayList<Integer>();
-        for (int i = 0; i < colors.size() + 1; i++) {
-            integers.add(i);
-        }
-        System.out.println(compare(colors, integers));
+        System.out.println(colors.equals(reversedColors));
         
-        // #12
-        integers.clear();
-        System.out.println(integers.size());
+        System.out.println("");
 
+        // #12
+        System.out.println("TASK 12:");
+
+        reversedColors.clear();
+        System.out.println(reversedColors.size());
+
+        System.out.println("");
         // #13
-        System.out.println(integers.isEmpty());
+        System.out.println("TASK 13:");
+
+        System.out.println(reversedColors.isEmpty());
         System.out.println(colors.isEmpty());
 
+        System.out.println("");
+
         // #14
+        System.out.println("TASK 14:");
+
         colors.ensureCapacity(10);
         System.out.println(colors);
 
-    }
-    // #14
-    public static<T> ArrayList<T> increaseCapacity(ArrayList<T> array, int newCapacity){
-        // Function increase capacity of the given generic arraylist
-        // Fills new slots with null
-        int currentCapacity = array.size();
-        ArrayList<T> newArray = new ArrayList<T>(newCapacity);
+        System.out.println("");
 
-        for (int i = 0; i < currentCapacity; i++) {
-            newArray.add(i, array.get(i));
-        }
-        for (int i = currentCapacity; i < newCapacity; i++) {
-            newArray.add(i, null);
-        }
-        return newArray;
-    }
-    // #11
-    public static<T, E> boolean compare(ArrayList<T> array1, ArrayList<E> array2){
-        // Function compare two generic arraylists by their lengths
-        // Return TRUE if length of the first arraylist is longer than second arraylist
-        // In other cases return FALSE
-        int length1 = array1.size();
-        int length2 = array2.size();
-        if(length1 >= length2) return true;
-        else return false;
-    }
-    // #10
-    public static<T> ArrayList<T> reverse(ArrayList<T> array){
-        int length = array.size();
-        if(length != 0){
-            ArrayList<T> reversedArray = new ArrayList<T>();
+        // #15
+        System.out.println("TASK 15:");
 
-            for (int j = length - 1; j >= 0; j--) {
-                reversedArray.add(array.get(j));
-            }
-            array = reversedArray; 
-        }
-        return array;
+        colors.ensureCapacity(colors.size());
+        System.out.println(colors);
     }
+
 }
