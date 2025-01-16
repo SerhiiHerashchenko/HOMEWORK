0e2c87e6c9461b11a1bf65cd2faf369123440fcd
SerhiiHerashchenko <serhii.herashchenko@stud.onu.edu.ua>
2025-01-16 14:36:31 +0200

    C#

---
+++ C#/test1/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] ints = new int[5];
+        for (int i = 0; i < ints.Length; i++)
+            ints[i] = 1;
+            
+        foreach (int num in ints)
+        {
+            Console.WriteLine( num + " Hello, World!");
+        }
+    }
+}
