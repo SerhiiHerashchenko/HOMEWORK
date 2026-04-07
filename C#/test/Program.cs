95116622c6c2846252b72c5e94afc1616e88ab90
Serhii Herashchenko <serhii.herashchenko@stud.onu.edu.ua>
2026-04-08 00:27:03 +0300

    1

--- C#/test/Program.cs
+++ C#/test/Program.cs
@@ -2,13 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        int[] ints = new int[5];
-        for (int i = 0; i < ints.Length; i++)
-            ints[i] = 1;
-            
-        foreach (int num in ints)
+        uint number = 22U;
+
+        string numberStr = number.ToString().Trim();
+        int count = 0;
+
+
+        for (int i = 1; i < numberStr.Length; i++)
         {
-            Console.WriteLine( num + " Hello, World!");
+            System.Console.WriteLine(numberStr[i - 1]);
+            System.Console.WriteLine(numberStr[i]);
+            if (numberStr[i - 1] == numberStr[i] && numberStr[i] == '2')
+                count++;
         }
+
+        Console.WriteLine(number + " " + count);
     }
 }
